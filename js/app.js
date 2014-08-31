21260d46d5ae346d3ac2138635c8f28e88abaf62
Steve Patterson <spatte21@btinternet.com>
2014-08-31 21:23:38 +0100

    Added weather imaged for display page

--- js/app.js
+++ js/app.js
@@ -2,52 +2,54 @@ var app = angular.module('app', ['ui.router', 'ngResource']);
 
 app.constant('coralReefUrl', 'http://coral-reef.azurewebsites.net');
 
-app.config(function($stateProvider) {
+app.config(function ($stateProvider, $locationProvider) {
 
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'home.html',
-            controller: function($scope, $interval, Candidates) {
-                var repeater;
-                repeater = $interval(function() {
-                    Candidates.query(function(data) {
-                        $scope.candidates = data;
-                        $scope.now = moment();
-                    });
-                }, 15000);
+  $locationProvider.html5Mode(true);
 
-                $scope.published = function(publishTime) {
-                    return moment(publishTime).fromNow();
-                }
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html',
+      controller: function ($scope, $interval, Candidates) {
+        var repeater;
+        repeater = $interval(function () {
+          Candidates.query(function (data) {
+            $scope.candidates = data;
+            $scope.now = moment();
+          });
+        }, 15000);
 
-                $scope.testResults = function(testing) {
-                    var results = 'n/a'
-                    if (testing.started !== null) {
-                        var test = testing.tests[0];
-                        results = test.module + ' - tests (' + test.stats.tests + '), passes(' + test.stats.passes + '), fails (' + test.stats.failures + ')';
-                    }
-                    return results;
-                }
+        $scope.published = function (publishTime) {
+          return moment(publishTime).fromNow();
+        }
 
-                $scope.stopRefresh = function() {
-                    if (angular.isDefined(repeater)) {
-                        $interval.cancel(repeater);
-                        repeater = undefined;
-                    }
-                };
+        $scope.testResults = function (testing) {
+          var results = 'n/a'
+          if (testing.started !== null) {
+            var test = testing.tests[0];
+            results = test.module + ' - tests (' + test.stats.tests + '), passes(' + test.stats.passes + '), fails (' + test.stats.failures + ')';
+          }
+          return results;
+        }
 
-                $scope.$on('$destroy', function() {
-                    $scope.stopRefresh();
-                });
-            }
-        })
-        .state('display', {
-            url: '/display',
-            templateUrl: 'display.html'
+        $scope.stopRefresh = function () {
+          if (angular.isDefined(repeater)) {
+            $interval.cancel(repeater);
+            repeater = undefined;
+          }
+        };
+
+        $scope.$on('$destroy', function () {
+          $scope.stopRefresh();
         });
+      }
+    })
+    .state('display', {
+      url: '/display',
+      templateUrl: 'display.html'
+    });
 
 })
-.run(function($state) {
+  .run(function ($state) {
     $state.transitionTo('home');
-});
+  });
