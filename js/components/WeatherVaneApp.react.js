1b873e0a4dd3eaa8d165d23691f56a3d6764a20a
Steve Patterson <spatte21@btinternet.com>
2014-11-17 13:29:31 +0000

    Going to handle missing data with some routes as current approach breaks Foundation tabs

--- js/components/WeatherVaneApp.react.js
+++ js/components/WeatherVaneApp.react.js
@@ -39,20 +39,13 @@ var WeatherVaneApp = React.createClass({
 
   render: function() {
     var buildList = this.state.buildList;
-console.log(buildList);
     var selectedBuild = this.state.selectedBuild;
 
     return <div className='row weather-vane'>
-      { buildList.list && buildList.list.length > 0 ?
-        <div>
-          <BuildList builds={buildList} />
-          <BuildDetails build={selectedBuild} />
-        </div>
-        :
-        <div className='small-12 medium-12 no-builds'>
-          <img src='/img/no_builds.jpg' />
-        </div>
-      }
+      <div>
+        <BuildList builds={buildList} />
+        <BuildDetails build={selectedBuild} />
+      </div>
     </div>;
   }
 
